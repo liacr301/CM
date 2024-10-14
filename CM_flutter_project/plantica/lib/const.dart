c2f9e84561efbe638c5368abb3d32bb11f10bb33
Lia Cardoso <lia.cardoso@ua.pt>
2024-10-14 23:53:58 +0100

    home page draft

---
+++ CM_flutter_project/plantica/lib/const.dart
@@ -0,0 +1,20 @@
+class PlantItem {
+  String img;
+  String name;
+  String scientificName;
+  String state;
+  PlantItem({required this.name, required this.img, required this.scientificName, required this.state});
+}
+
+
+List<PlantItem> myPlants = [
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Healthy'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Needs water'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Too much water'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Healthy'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Healthy'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Healthy'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Healthy'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Healthy'),
+  PlantItem(name: 'Moth Orchids', img: 'https://m.planfor.pt/Donnees_Site/Produit/Images/1751/orquidea-borboleta-malva-phalaenopsis_PT_500_0024283.jpg', scientificName: 'Phalaenopsis amabilis', state: 'Healthy'),
+];
--- CM_flutter_project/plantica/lib/screens/homepage.dart
+++ CM_flutter_project/plantica/lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantica/const.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -44,22 +45,106 @@ class _HomeState extends State<Home> {
               },
             ),
           ]),
-          body: 
-          Container(
-            decoration: BoxDecoration(
-              
-            ),
-            child: Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.only(left: 16, right: 16, bottom: 36),
+      body: Container(
+        decoration: BoxDecoration(),
+        child: Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(20),
+                itemCount: myPlants.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            width: 80,
+                            height: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(100),
+                                bottomLeft: Radius.circular(100),
+                                topRight: Radius.circular(100),
+                                bottomRight: Radius.circular(100),
+                              ),
+                              image: DecorationImage(
+                                image: NetworkImage(myPlants[index].img),
+                                fit: BoxFit.cover,
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.2),
+                                  spreadRadius: 1,
+                                  blurRadius: 6,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 12),
+                          // Informações da planta
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  myPlants[index].name,
+                                  style: const TextStyle(
+                                    fontFamily: 'Raleway',
+                                    color: Color.fromARGB(255, 24, 48, 3),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Text(
+                                  myPlants[index].scientificName,
+                                  style: const TextStyle(
+                                    fontFamily: 'Raleway',
+                                    color: Color.fromARGB(255, 24, 48, 3),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      // Adiciona espaçamento interno no conteúdo
+                                      padding: const EdgeInsets.only(left: 8, right: 8),
+                                      decoration: BoxDecoration(
+                                        color:
+                                            myPlants[index].state == 'Healthy'
+                                                ? Colors.green[100]
+                                                : myPlants[index].state ==
+                                                        'Needs water'
+                                                    ? Colors.red[300]
+                                                    : Colors.blue[200],
+                                        borderRadius: BorderRadius.circular(
+                                            20), 
+                                      ),
+                                      child: Text(
+                                        myPlants[index].state,
+                                        style: const TextStyle(
+                                          fontFamily: 'Raleway',
+                                          color: Colors
+                                              .black, // Definir a cor do texto conforme necessário
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 4),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Espaço entre os itens
+                      const SizedBox(height: 16),
+                    ],
                   );
-                } 
-              )
-            ),
-          ),
+                })),
+      ),
     ));
   }
 }
